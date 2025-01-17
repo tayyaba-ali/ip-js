153269faf17948ccc84eb4134c6b758229a2b84b
tayyaba-ali <ta6402792@gmail.com>
2025-01-17 13:09:05 +0500

    till and or operator

---
+++ app.js
@@ -0,0 +1,63 @@
+// for (var i = 10; i >= 1; i--) {
+//   console.log(i);
+
+//   for (var j = 5; j >= 1; j--) {
+//     console.log(i- j);
+//   }
+// }
+
+// console.log();
+
+// =
+// ==
+// !=
+// ===
+// !==
+// console.log(6!=5);
+
+// console.log("5"!== "5");
+
+// console.log(Boolean([]));
+
+// truthy value
+
+// falsy value
+// ""
+// 0
+// undefined
+// null
+// false
+
+// if else
+
+// var age = 12
+
+// if (18<=age) {
+//   console.log("you are allowed to have a ride");
+// }
+// else{
+// alert("bare hokr ao")
+
+// }
+// var studentNames = ["maryam", "ushna", "aiman", "tayyaba"];
+// console.log (studentNames.slice (3,4))
+
+
+
+// var ticketNumber = 2
+
+// if (guessingNumber === ourNum || ticketNumber ==6) {
+//   console.log("bariyaaa");
+// } else {
+//   console.log("try again");
+// }
+
+
+
+// console.log(2<3 && 3>9 || 6<33 );
+
+var guessingNumber = 5;
+var ourNum = "4";
+console.log(6>3  && ourNum <=5 || (5>6)&& 7>3 );
+
+//  
