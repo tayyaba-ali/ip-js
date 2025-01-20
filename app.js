0fde1e86caabeed86353f7bec0c22ddd103aa6e1
tayyaba-ali <ta6402792@gmail.com>
2025-01-20 13:14:37 +0500

    half revision

--- app.js
+++ app.js
@@ -56,8 +56,77 @@
 
 // console.log(2<3 && 3>9 || 6<33 );
 
-var guessingNumber = 5;
-var ourNum = "4";
-console.log(6>3  && ourNum <=5 || (5>6)&& 7>3 );
+// var guessingNumber = 5;
+// var ourNum = "4";
+// console.log(6>3  && ourNum <=5 || (5>6)&& 7>3 );
 
-//  
+// //  
+
+
+// today class coding
+
+// var userName =   prompt("What's your name")
+// console.log(userName);
+
+// var guessNumber = 10
+
+// var userNumber = +prompt("Guess number")
+
+// // console.log(typeof userNumber);
+
+// if(userNumber===guessNumber){
+//     console.log("you guessed it right");
+
+// }
+// else{
+//     console.log("Try again");
+
+// }
+
+// var studentRollNo =   prompt("what's your roll no")
+
+// console.log(studentRollNo);
+
+// var myName;
+
+// console.log(typeof myName);
+
+// alert("thanks for visiting")
+
+// "hsdflhsdflhdsfl787"
+
+// 546545646556
+// boolean true false
+
+// var studentNumberIsPresent  = 35464654
+
+// console.log(4+5-5*(4-3));
+
+// console.log(45/2);
+
+// console.log(105%3);
+
+// var firstName  = "45"
+// var lastName = "45"
+
+// console.log(firstName - lastName);
+
+// var userAddress =  +prompt("what's your address")
+
+// console.log(4==="4");
+
+// console.log(Boolean(-1));
+
+// var age = +prompt("what your age");
+// var weight = +prompt("what's your weight");
+
+// if (age >= 18 || weight === 40  ) {
+//   console.log("you are allowed");
+// } else {
+//   if (weight !== 40) {
+//     console.log("your weight is not suitable");
+//   } else {
+//     console.log("bare hokr ao");
+//   }
+// }
+
